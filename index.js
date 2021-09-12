bca86e51f593221e041ec9c1f2fd093ab3c28bec
Abdifatah <abdifatah.101@outlook.com>
2021-09-12 20:18:30 +0100

    added dependencies

---
+++ index.js
@@ -0,0 +1,9 @@
+//dependencies
+
+const path = require('path');
+const express = require('express');
+const exphbs = require('express-handlebars');
+const routes = require('./controllers');
+const helpers = require('./utils/helpers');
+const sequelize = require('./config/connection');
+
